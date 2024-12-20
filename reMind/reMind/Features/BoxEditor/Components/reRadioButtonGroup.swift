3a7b334ea3660915e3a359eac1cc1ae66e7bb412
João Pedro Volponi <jpvrosada@hotmail.com>
2024-12-19 21:38:35 -0300

    Boxes

--- reMind/reMind/Features/BoxEditor/Components/reRadioButtonGroup.swift
+++ reMind/reMind/Features/BoxEditor/Components/reRadioButtonGroup.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct reRadioButtonGroup: View {
     @State var title: String = ""
     @Binding var currentSelection: Int
+    @Environment(\.colorScheme) var scheme
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
@@ -18,17 +19,43 @@ struct reRadioButtonGroup: View {
                     .font(.body)
                     .fontWeight(.bold)
             }
-
+            
             HStack(spacing: 20) {
                 reRadioButton(tag: 0,
                               currentSelection: $currentSelection,
                               color: Palette.mauve.render)
+                .overlay {
+                    if currentSelection == 0 {
+                        Circle()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(.white)
+                        
+                    }
+                }
                 reRadioButton(tag: 1,
                               currentSelection: $currentSelection,
                               color: Palette.lavender.render)
+                
+                .overlay {
+                    if currentSelection == 1 {
+                        Circle()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(.white)
+                        
+                    }
+                }
                 reRadioButton(tag: 2,
                               currentSelection: $currentSelection,
                               color: Palette.aquamarine.render)
+                
+                .overlay {
+                    if currentSelection == 2 {
+                        Circle()
+                            .frame(width: 25, height: 25)
+                            .foregroundStyle(.white)
+                        
+                    }
+                }
             }
         }
         .frame(maxWidth: .infinity, alignment: .leading)
